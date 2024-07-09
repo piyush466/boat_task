c3de32cf716d120b08a6d1a5548d240a9d89467c
piyush dravyakar <piyushdravyakar69@gmail.com>
2024-07-09 17:03:40 +0530

    Adding new YML file

--- test_case/conftest.py
+++ test_case/conftest.py
@@ -1,13 +1,13 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
+# from webdriver_manager.chrome import ChromeDriverManager
 
 
 @pytest.fixture()
 def setup():
-    # options = Options()
-    # options.add_argument("--headless")
+    options = Options()
+    options.add_argument("--headless")
     driver = webdriver.Chrome()
     driver.get("https://www.boat-lifestyle.com/")
     driver.implicitly_wait(10)
